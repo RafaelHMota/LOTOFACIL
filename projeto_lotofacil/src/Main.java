6963548ed11849bab75b1cbe53bf7e3b2b5bd2d1
Rafael <144626992+RafaelHMota@users.noreply.github.com>
2024-04-03 19:11:58 -0300

    Update Main.java
    Finalizado!!

--- projeto_lotofacil/src/Main.java
+++ projeto_lotofacil/src/Main.java
@@ -36,12 +36,12 @@ public class Main {
         scanner.close();
     }
 
-    // Método para apostar de 0 a 100
+    // Metodo para apostar de 0 a 100
     public static void apostarDe0a100(Scanner scanner, Random random) {
         System.out.print("Digite um numero de 0 a 100:");
         int aposta = scanner.nextInt();
 
-        // Verifica se a aposta está dentro do intervalo válido
+        // Verifica se a aposta esta dentro do intervalo valido
         if (aposta < 0 || aposta > 100) {
             System.out.println("Aposta invalida.");
             return;
@@ -56,28 +56,29 @@ public class Main {
             System.out.println("Que pena! O numero sorteado foi:" + numeroSorteado + ".");
         }
     }
-
+    // Metodo para apostar de A a Z
     public static void apostaDeAaZ(Scanner scanner) {
         System.out.print("Digite uma letra de A a Z:");
         char aposta =
-                Character.toUpperCase(scanner.next().charAt(0));
+        Character.toUpperCase(scanner.next().charAt(0));
+        // Verifica se a aposta foi correta
         if (!Character.isLetter(aposta)) {
             System.out.println("Aposta invalida.");
             return;
         }
         char letraPremiada = 'R';
-
+        // Verifica se a aposta foi correta
         if (aposta == letraPremiada) {
             System.out.println("Voce ganhou R$ 500,00 reais.");
         } else {
             System.out.println("Que pena! A letra sorteada foi" + letraPremiada + ".");
         }
     }
-
+    // Metodo para apostar em Par ou Impar
     public static void apostarParouImpar(Scanner scanner) {
         System.out.println("Digite um numero inteiro: ");
         int numero = scanner.nextInt();
-
+        // Verifica se o numero e par ou impar
         if (numero % 2 == 0) {
             System.out.println("Voce ganhou R$ 100,00 reais.");
         } else {
